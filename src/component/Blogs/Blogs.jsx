3db4ddbe06019008eb882e16587867cede0998ff
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-10-22 14:27:27 +0600

    Blogs API add and fetch the data.

---
+++ src/component/Blogs/Blogs.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  return (
+    <div>
+      <h2>Blogs : {blogs.length}</h2>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold ">Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
+// https://i.ibb.co.com/1KKYYVj/flat-lay-desk-dark-concept-with-copy-space.jpg
